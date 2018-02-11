6b92ebcf589565615ac3fdc737e7560450e225c0
Ali-aqrabawi <ali_aqrabawi@yahoo.com>
2018-02-11 17:16:04 +0200

    Update vlan_autoConf.py

--- vlan_autoConf.py
+++ vlan_autoConf.py
@@ -1,7 +1,9 @@
-from vlan_model import Vlan
+from vlan_model import Vlan,Switch,Synchronizer
 from switchs_config_file import Switchs_IPs ,Username , Password
 import paramiko
 import logging
+import threading
+from threading import Thread
 
 __name__ = "Vlan_AutoConf"
 logging.basicConfig(level=logging.INFO)
@@ -10,26 +12,57 @@ logger = logging.getLogger(__name__)
 
 
 
-def Collect_vlans() :
-    logger.info(' Start synching Vlans from : %s',Switchs_IPs)
-
-    for switch in Switchs_IPs :
-        conn = paramiko.SSHClient()
-        conn.set_missing_host_key_policy(paramiko.AutoAddPolicy())
-        conn.connect(ip, port=22, username=UN, password=PW)
-        remote = conn.invoke_shell()
 
 
+def userprompt():
+    while True:
+
+        x = input("Enter option\n" + "1. add vlan\n" + "2. view vlans\n" + " : ")
+
+        if int(x) == 1:
+            id = input("enter vlan id : ")
+            name = input("enter vlan name : ")
+            description = input("enter vlan descriptoin : ")
+            sessionVlan = Vlan()
+            sessionVlan.id = id
+            sessionVlan.name = name
+            sessionVlan.description = description
+
+            sessionVlan.AddVlan()
+            print("##########################################################")
+
+        elif int(x) == 2:
+            sessionVlan = Vlan()
+
+            sessionVlan.ViewVlans()
+            print("##########################################################")
+
+        elif int(x) == 3:
+            sessionVlan = Vlan()
+
+
+            sessionVlan.DeleteVlan('2')
+            print("##########################################################")
+
+        else:
+            print("enter valid value please")
+            print("##########################################################")
+
+
+list_of_switches_object = []
+for ip in Switchs_IPs :
+    switch = Switch(ip,Username,Password)
+    list_of_switches_object.append(switch)
+
+sync = Synchronizer(list_of_switches_object)
+Thread(target = sync.run()).start()
+Thread(target = userprompt()).start()
 
 
 
 
 
 
-#conn = paramiko.SSHClient()
-#conn.set_missing_host_key_policy(paramiko.AutoAddPolicy())
-#conn.connect(ip, port=22, username=UN, password=PW)
-#remote = conn.invoke_shell()
 
 
 
@@ -39,34 +72,3 @@ def Collect_vlans() :
 
 
 
-
-
-
-
-Collect_vlans()
-while True :
-
-
-    x = input("Enter option\n" + "1. add vlan\n" + "2. view vlans\n" + " : ")
-
-    if int(x) == 1 :
-        id = input("enter vlan id : ")
-        name = input("enter vlan name : ")
-        description = input("enter vlan descriptoin : ")
-        sessionVlan = Vlan()
-        sessionVlan.id = id
-        sessionVlan.name = name
-        sessionVlan.description = description
-
-        sessionVlan.AddVlan()
-        print("##########################################################")
-
-    elif int(x) == 2 :
-        sessionVlan = Vlan()
-
-        sessionVlan.ViewVlans()
-        print("##########################################################")
-
-    else :
-        print("enter valid value please")
-        print("##########################################################")
