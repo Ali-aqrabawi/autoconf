fca4bd06b0c641f5f3318d36eaa99c51cb4de0fc
Ali-aqrabawi <ali_aqrabawi@yahoo.com>
2018-02-11 22:13:17 +0200

    Update switchs_config_file.py

--- switchs_config_file.py
+++ switchs_config_file.py
@@ -1,8 +1,9 @@
 
 #add your devices ips in the below list :
 
-Switchs_Ips = ['192.168.1.1' , '192.168.1.2' , '192.168.1.3']
+Switchs_IPs = ['10.48.76.12']
 
 #enter SSH credintials :
-Username = ""
-Password = ""
+Username = "cisco"
+Password = "cisco"
+en_pass = "ww"
